1b5ce072a8ec358dfa13a1849c1a62cee09cfd7c
nekofood <sonicroblox@gmail.com>
2023-04-05 15:47:19 +0800

    remove one line from sql

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const db = mysql.createConnection({
     host : process.env.SQL_HOST,
     user : process.env.SQL_USERNAME,
     password : process.env.SQL_PASSWORD,
-    database : ''
 });
 
 db.connect((err) => {
