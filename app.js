a32298004d73722ef43fd2a87b35452b41728c09
dyitsme <denzel_bryan_yongco@dlsu.edu.ph>
2023-04-05 15:19:28 +0800

    mvc and ejs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
+const movieRoutes = require('./routes/movieRoutes');
 
 const db = mysql.createConnection({
     host : 'localhost',
@@ -7,7 +8,7 @@ const db = mysql.createConnection({
     password : '',
     database : ''
 });
-//connect
+
 db.connect((err) => {
     if(err){
         throw err;
@@ -16,7 +17,10 @@ db.connect((err) => {
 });
 
 const app = express();
-
+app.set('view engine', 'ejs');
 app.listen('3000', () => {
     console.log("Server started on port 3000");
 });
+
+// movie routes
+app.use('/', movieRoutes);
