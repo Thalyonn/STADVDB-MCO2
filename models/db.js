5020162a4d551a752f61ab828e918015bb426078
nekofood <sonicroblox@gmail.com>
2023-04-05 15:59:29 +0800

    added update function

--- models/db.js
+++ models/db.js
@@ -16,10 +16,17 @@ const database = {
 		});
 	},
 
-	selectAllFromNode: function() {
+	selectAll: function() {
 		db.query("SELECT * FROM node", (err, result, fields) => {
 			if(err) throw err;
 			return result;
+		})
+	}
+
+	updateOne: function(id, field, value) {
+		db.query("UPDATE node SET " + field + " = " + value + " WHERE id = " + id, (err, result) => {
+			if(err) throw err;
+			console.log(result.affectedRows + " row(s) updated");
 		})
 	}
 }
