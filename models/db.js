85426fcd582445ed4e6726f180b69b58261b1dda
nekofood <sonicroblox@gmail.com>
2023-04-05 16:21:25 +0800

    hotfix sql db info in connection

--- models/db.js
+++ models/db.js
@@ -4,6 +4,7 @@ const db = mysql.createConnection({
 	host : process.env.SQL_HOST,
 	user : process.env.SQL_USERNAME,
 	password : process.env.SQL_PASSWORD,
+	databse: process.env.SQL_DATABSE
 });
 
 const database = {
