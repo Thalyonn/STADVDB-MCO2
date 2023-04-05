f484d474bea557a44cc6c7999ba20647d8d31ab9
nekofood <sonicroblox@gmail.com>
2023-04-05 16:11:33 +0800

    added insert function

--- models/db.js
+++ models/db.js
@@ -21,10 +21,31 @@ const database = {
 			if(err) throw err;
 			return result;
 		})
-	}
+	},
+
+	insertOne: function(name, year, rating, genres) {
+		if(genres.isArray()){
+			count = 0;
+			//insert a row for each genre
+			for (i in genres) {
+				db.query("INSERT INTO node (name, year, rating, genre) VALUES ('" + name + "', '" + year + "', '" + rating + "', '" + i + "')", 
+					(err, result) => {
+						if(err) throw err;
+						count++;
+				});
+			}
+			console.log("Inserted " + count + " rows");
+		}
+		else {
+			db.query("INSERT INTO node (name, year, rating, genre) VALUES ('" + name + "', '" + year + "', '" + rating + "', '" + genres + "')", (err, result) => {
+				if(err) throw err;
+				console.log("Inserted a row");
+			});
+		}
+	},
 
 	updateOne: function(id, field, value) {
-		db.query("UPDATE node SET " + field + " = " + value + " WHERE id = " + id, (err, result) => {
+		db.query("UPDATE node SET '" + field + "' = '" + value + "' WHERE id = " + id, (err, result) => {
 			if(err) throw err;
 			console.log(result.affectedRows + " row(s) updated");
 		})
