bef2d5fd0c09af0755b65551d39ad9ac01482fcc
nekofood <sonicroblox@gmail.com>
2023-04-07 17:07:46 +0800

    .env hotfix T_T

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const db = require('./models/db.js')
 
 const app = express();
 app.set('view engine', 'ejs');
-app.listen('3000', () => {
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log("Server started on port " + process.env.PORT);
 });
 
 // movie routes
