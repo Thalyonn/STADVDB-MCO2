98ccb7f12708d4fd6c38ac6e4c8e2ce7237f3883
nekofood <sonicroblox@gmail.com>
2023-04-10 10:45:16 +0800

    added try-catch

--- models/db.js
+++ models/db.js
@@ -16,9 +16,13 @@ const database = {
 
 	selectAll: async function(node) { //Where "node" is a mysql2 connectionPool
 		const promisePool = node.promise();
-		await promisePool.query(start_transac);
-		const [results, fields] = await promisePool.query("SELECT * FROM node")
-		await promisePool.query("COMMIT");
+		try {
+			await promisePool.query(start_transac);
+			const [results, fields] = await promisePool.query("SELECT * FROM node")
+			await promisePool.query("COMMIT");
+		} catch (e) {
+			console.error(e);
+		}
 
 		return results;
 	},
@@ -34,27 +38,38 @@ const database = {
 
 	selectYearRange: async function(node, start, end) {
 		const promisePool = node.promise()
-		await promisePool.query(start_transac);
-		const [results, fields] = await promisePool.query("SELECT * FROM node WHERE year BETWEEN " + start + " AND " + end)
-		await promisePool.query("COMMIT");
-
+		try {
+			await promisePool.query(start_transac);
+			const [results, fields] = await promisePool.query("SELECT * FROM node WHERE year BETWEEN " + start + " AND " + end)
+			await promisePool.query("COMMIT");
+		} catch e() {
+			console.error(e);
+		}
 		return results;
 	},
 
 	insertOne: async function(node, name, year, rating, genres) {
 		const promisePool = node.promise()
-		await promisePool.query(start_transac);
-		await promisePool.query("INSERT INTO node (name, year, rating, genre) VALUES ('" + name + "', '" + year + "', '" + rating + "', '" + genres + "')")
-		console.log("Inserted a row");
-		await promisePool.query("COMMIT");
+		try {
+			await promisePool.query(start_transac);
+			await promisePool.query("INSERT INTO node (name, year, rating, genre) VALUES ('" + name + "', '" + year + "', '" + rating + "', '" + genres + "')")
+			console.log("Inserted a row");
+			await promisePool.query("COMMIT");
+		} catch (e) {
+			console.error(e);
+		}
 	},
 
 	updateOne: async function(node, id, field, value) {
 		const promisePool = node.promise()
-		await promisePool.query(start_transac)
-		const [results, fields] = await promisePool.query("UPDATE node SET '" + field + "' = '" + value + "' WHERE id = " + id)
-		console.log(results.affectedRows + " row(s) updated");
-		await promisePool.query("COMMIT")
+		try {
+			await promisePool.query(start_transac)
+			const [results, fields] = await promisePool.query("UPDATE node SET '" + field + "' = '" + value + "' WHERE id = " + id)
+			console.log(results.affectedRows + " row(s) updated");
+			await promisePool.query("COMMIT")
+		} catch (e) {
+			console.error(e);
+		}
 
 	},
 
