09ea5691f9cc17c9f043401c58b5d2edb1ea8989
John Wesley Abe <68657618+Frieya@users.noreply.github.com>
2023-04-17 06:34:45 +0800

    Update update_log.sql

--- sql/update_log.sql
+++ sql/update_log.sql
@@ -1,16 +1,5 @@
 USE node_all;
 DELIMITER $$
-CREATE TRIGGER movie_insert 
-    AFTER INSERT 
-    ON node_all.node FOR EACH ROW 
-BEGIN
- INSERT INTO logs.log(transaction_date, action, row_id, name, year, rating, genre) 
- VALUES (NOW(), "INSERT", NEW.id, NEW.name, NEW.year, NEW.rating, NEW.genre);
-END$$
-DELIMITER ;
-
-USE node_all;
-DELIMITER $$
 CREATE TRIGGER movie_update 
     AFTER UPDATE ON node_all.node
     FOR EACH ROW 
