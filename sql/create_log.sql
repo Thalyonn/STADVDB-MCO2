a753e6f69437cef011345876cfda4a448447cd83
nekofood <sonicroblox@gmail.com>
2023-04-16 12:30:08 +0800

    modify columns

--- sql/create_log.sql
+++ sql/create_log.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE IF NOT EXISTS logs;
 
 CREATE TABLE logs.log (
-	transaction_id int NOT NULL,
+	transaction_id int NOT NULL AUTO INCREMENT,
+	row_id int,
 	modified_column ENUM('name', 'year', 'rating', 'genre'),
 	old_value VARCHAR(100),
 	new_value VARCHAR(100),
