bd7a3ba7205ec51df34ef2f591b94fd4792c0020
John Wesley Abe <68657618+Frieya@users.noreply.github.com>
2023-04-16 14:34:44 +0800

    Update create_log.sql

--- sql/create_log.sql
+++ sql/create_log.sql
@@ -2,6 +2,8 @@ CREATE DATABASE IF NOT EXISTS logs;
 
 CREATE TABLE logs.log (
 	transaction_id int NOT NULL AUTO INCREMENT,
+	transaction_date DATETIME DEFAULT NULL,
+	action VARCHAR(50) DEFAULT NULL,
 	row_id INT,
 	name VARCHAR(100),
 	year INT,
