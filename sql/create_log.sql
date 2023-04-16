0edd3cf44d26b3f4aecbd636063bd9647e1442c0
nekofood <sonicroblox@gmail.com>
2023-04-16 12:22:42 +0800

    create log table

---
+++ sql/create_log.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS logs;
+
+CREATE TABLE logs.log (
+	transaction_id int NOT NULL,
+	modified_column ENUM('name', 'year', 'rating', 'genre'),
+	old_value VARCHAR(100),
+	new_value VARCHAR(100),
+	PRIMARY KEY (transaction_id)
+)
